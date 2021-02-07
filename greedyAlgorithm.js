55bca7e19eb76500df51ddc2d106ad935609f13d
Artem Gusev <gusev2014russia@mail.ru>
2021-02-07 13:24:44 +0300

    Refactor code for greedy algorithm implementation

--- greedyAlgorithm.js
+++ greedyAlgorithm.js
@@ -1,3 +1,9 @@
+/**
+ * Intersection `Set` operation
+ * @param {Set} setA - first set
+ * @param {Set} setB - second set
+ * @return {Set<any>} - result of intersection operation
+ */
 function intersection(setA, setB) {
   const _intersection = new Set();
   for (let elem of setB) {
@@ -6,6 +12,12 @@ function intersection(setA, setB) {
   return _intersection;
 }
 
+/**
+ * Difference `Set` operation
+ * @param {Set} setA - first set
+ * @param {Set} setB - second set
+ * @return {Set<any>} - result of difference operation
+ */
 function difference(setA, setB) {
   const _difference = new Set(setA);
   for (let elem of setB) {
@@ -13,7 +25,6 @@ function difference(setA, setB) {
   }
   return _difference;
 }
-
 
 
 let statesNeeded = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az']);
@@ -27,21 +38,28 @@ const stations = new Map([
 
 const finalStations = new Set();
 
-while (statesNeeded.size) {
-  let bestStation = null;
-  let statesCovered = new Set();
+/**
+ * Greedy algorithm implementation. For task with stations.
+ * @param {Set} statesNeeded
+ * @param {Set} stations
+ * @return {Set<any>} - result of greedy algorithm
+ */
+function greedyAlgorithm(statesNeeded, stations) {
+  while (statesNeeded.size) {
+    let bestStation = null;
+    let statesCovered = new Set();
 
-  stations.forEach((statesForStation, station) => {
-    const covered = intersection(statesNeeded, statesForStation);
-    if (covered.size > statesCovered.size) {
-      bestStation = station;
-      statesCovered = covered;
-    }
-  });
+    stations.forEach((statesForStation, station) => {
+      const covered = intersection(statesNeeded, statesForStation);
+      if (covered.size > statesCovered.size) {
+        bestStation = station;
+        statesCovered = covered;
+      }
+    });
 
-  statesNeeded = difference(statesNeeded, statesCovered);
-  finalStations.add(bestStation);
+    statesNeeded = difference(statesNeeded, statesCovered);
+    finalStations.add(bestStation);
+  }
+  return finalStations;
 }
-
-console.log(finalStations)
-
+console.log(greedyAlgorithm(statesNeeded, stations));
