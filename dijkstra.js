fda63e4de697571248de6e86df33e529ea698887
Artem Gusev <gusev2014russia@mail.ru>
2021-02-07 13:17:38 +0300

    Refactor code for implementation of Dijkstra algorithm

--- dijkstra.js
+++ dijkstra.js
@@ -1,3 +1,64 @@
+/**
+ * @description Implementation of Dijkstra algorithm.
+ *
+ * >Result of this implementation contains inside `parents` hash-table
+ * @param {Map} graph - `graph` hash-table
+ * @param {Map} costs - `costs` hash-table
+ * @param {Map} parents - `parents` hash-table
+ * @return {void}
+ */
+function dijkstra(graph, costs, parents) {
+  const processed = [];
+
+  /**
+   * Find lowest cost node from the `costs` hash-table and return it `key` value.
+   * @param {Map} costs - `costs` hash-table
+   * @return {string} - key value of the lowest cost node
+   */
+  function findLowestCostNode(costs) {
+    let lowestCost = Infinity;
+    let lowestCostNode = null;
+    costs.forEach((val, key) => {
+      if (lowestCost > val && !processed.includes(key)) {
+        lowestCost = val;
+        lowestCostNode = key;
+      }
+    })
+    return lowestCostNode;
+  }
+
+  let node = findLowestCostNode(costs);
+  while (node) {
+    let cost = costs.get(node);
+    let neighbors = graph.get(node);
+    for (let n in neighbors) {
+      let newCost = cost + neighbors[n];
+      if (costs.get(n) > newCost) {
+        costs.set(n, newCost);
+        parents.set(n, node);
+      }
+    }
+    processed.push(node);
+    node = findLowestCostNode(costs);
+  }
+}
+
+/**
+ * Sets path for lowest cost path.
+ * @param {Map} parents - `parents` hash-table
+ * @param {string} point - endpoint key value.
+ * @return {string} - forming path
+ */
+function getPath(parents, point) {
+  const path = [point];
+  while (point) {
+    const pointVal = parents.get(point);
+    path.unshift(pointVal);
+    point = parents.has(pointVal) ? pointVal : null;
+  }
+  return path.join(' => ')
+}
+
 const graph = new Map([
   [
     'start',
@@ -40,43 +101,5 @@ const parents = new Map([
   ['fin', null]
 ]);
 
-const processed = [];
-
-let node = findLowestCostNode(costs);
-while (node) {
-  let cost = costs.get(node);
-  let neighbors = graph.get(node);
-  for (let n in neighbors) {
-    let newCost = cost + neighbors[n];
-    if (costs.get(n) > newCost) {
-      costs.set(n, newCost);
-      parents.set(n, node);
-    }
-  }
-  processed.push(node);
-  node = findLowestCostNode(costs);
-}
-
+dijkstra(graph, costs, parents);
 console.log(getPath(parents, 'fin'));
-
-function getPath(parents, point) {
-  const path = [point];
-  while (point) {
-    const pointVal = parents.get(point);
-    path.unshift(pointVal);
-    point = parents.has(pointVal) ? pointVal : null;
-  }
-  return path.join(' => ')
-}
-
-function findLowestCostNode(costs) {
-  let lowestCost = Infinity;
-  let lowestCostNode = null;
-  costs.forEach((val, key) => {
-    if (lowestCost > val && !processed.includes(key)) {
-      lowestCost = val;
-      lowestCostNode = key;
-    }
-  })
-  return lowestCostNode;
-}
