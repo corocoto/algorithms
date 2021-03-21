7885ee73bbc3f7904903a4b3f1f0e7a0767f1987
Artem Gusev <gusev2014russia@mail.ru>
2021-03-21 19:04:21 +0300

    Adds additional interfaces for lru cache optimization

--- lru-cache.js
+++ lru-cache.js
@@ -1,7 +1,63 @@
+class ListNode {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = this.tail = null;
+  }
+
+  splice(node) {
+    /* одна нода в списке */
+    if (!node.prev && !node.next) {
+      this.head = this.tail = null;
+      /*первый элемент*/
+    } else if (!node.prev) {
+      this.head = node.next;
+      this.head.prev = null;
+      /* последний эелмент*/
+    } else if (!node.next) {
+      this.tail = node.prev;
+      this.tail.next = null;
+      /*не первая и не последняя нода */
+    } else {
+      const next = node.next;
+      const prev = node.prev;
+      prev.next = next;
+      next.prev = prev;
+      node.next = node.prev = null;
+    }
+    this.length--;
+  }
+
+  unshift(node) {
+    if (this.tail) {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    } else {
+      this.head = this.tail = node;
+    }
+    this.length++;
+  }
+
+  pop() {
+    const tail = this.tail;
+    this.splice(this.tail);
+    return tail;
+  }
+}
+
 class LRUCache {
   constructor(capacity) {
     this.capacity = capacity;
-    this.q = [];
+    this.q = new LinkedList();
     this.m = {};
   }
 
@@ -15,12 +71,13 @@ class LRUCache {
   }
 
   put(key, value) {
-    if(this.m[key]) {
-      this.q.splice(this.q.findIndex(node => node === this.m[key]), 1);
+    if (this.m[key]) {
+      this.q.splice(this.m[key]);
       this.m[key] = null;
     }
-    this.q.unshift({key, value});
-    this.m[key] = this.q[0];
+    const node = new ListNode(key, value);
+    this.q.unshift(node);
+    this.m[key] = node;
     if (this.q.length > this.capacity) {
       this.m[this.q.pop().key] = null;
     }
@@ -29,9 +86,9 @@ class LRUCache {
 
 
 const cache = new LRUCache(3);
-cache.put(1,1);
-cache.put(2,1);
+cache.put(1, 1);
+cache.put(2, 1);
 cache.get(2);
-cache.put(3,1);
-cache.put(4,1);
+cache.put(3, 1);
+cache.put(4, 1);
 console.log(cache.q)
